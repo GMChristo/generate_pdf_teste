bc323af199b5e6d9deae4435a3d4ea92c6186953
GMChristo <gabrielchristo@hotmail.com>
2021-06-23 16:55:48 -0300

    pequenas alterações de organização do codigo

--- lib/src/todo_list/todo_list_component.dart
+++ lib/src/todo_list/todo_list_component.dart
@@ -7,14 +7,14 @@ import 'package:angular_components/angular_components.dart';
 
 import 'todo_list_service.dart';
 
-//import 'package:pdf/pdf.dart';
+import 'package:pdf/pdf.dart';
+
 import 'package:pdf/widgets.dart' as pw;
 import 'dart:html' as html;
-
 import 'dart:js' as js;
 
-//import para uso do metodo com js
-//import '../interop/js_interop.dart';
+// import para uso do metodo com js
+import '../interop/js_interop.dart';
 
 @Component(
   selector: 'todo-list',
@@ -34,7 +34,9 @@ class TodoListComponent implements OnInit {
   final TodoListService todoListService;
   final DomSanitizationService sanitizer;
   
+  String namePdf = '';
   String text = '';
+  String testeHtml = '';
 
   TodoListComponent(this.todoListService, this.sanitizer);
 
@@ -42,33 +44,35 @@ class TodoListComponent implements OnInit {
   Future<Null> ngOnInit() async {
   }
 
-  Future<void> testePDF() async {
-    final pdf = pw.Document();
-    pdf.addPage(
-      pw.Page(
-        build: (pw.Context context) => pw.Center(
-          child: pw.Text(text),
-        ),
-      ),
-    );
-    var array = await pdf.save();
-    print(array);
-   final pdfFile = html.Blob(
-      <Uint8List>[Uint8List.fromList(array)],
-      'application/pdf',
-    );
-    final pdfUrl = html.Url.createObjectUrl(pdfFile);
-    final html.HtmlDocument doc = js.context['document'];
-    final link = html.AnchorElement(href: pdfUrl);
-    link.download = 'teste2.pdf';
-    doc.body?.append(link);
-    link.click();
-    link.remove();
+  // Future<void> testePDF() async {
+  //   final pdf = pw.Document();
+  //   pdf.addPage(
+  //     pw.Page(
+  //       build: (pw.Context context) => pw.Center(
+  //         child: pw.Text(text),
+  //       ),
+  //     ),
+  //   );
+  //   var array = await pdf.save();
+  //   print(array);
+  //  final pdfFile = html.Blob(
+  //     <Uint8List>[Uint8List.fromList(array)],
+  //     'application/pdf',
+  //   );
+  //   final pdfUrl = html.Url.createObjectUrl(pdfFile);
+  //   final html.HtmlDocument doc = js.context['document'];
+  //   final link = html.AnchorElement(href: pdfUrl);
+  //   link.download = '$namePdf.pdf';
+  //   doc.body?.append(link);
+  //   link.click();
+  //   link.remove();
+  // }
 
+  Future<void> testePDF() async {
     //Usando script js para salvar o arquivo, script adicionado no index e
     //criado js_interop.dar para fazer a ligação com o script
+    setText('$text', 10, 10);
+    savePdf('$namePdf.pdf');
 
-    //setText('teste!', 10, 10);
-    //savePdf('teste.pdf');
   }
 }
